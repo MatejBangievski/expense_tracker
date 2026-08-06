e3a435c7329cbe97c09a35ffc1fffd9b5b033e18
matej.bangievski <matej.bangievski@sorsix.com>
2026-08-06 15:43:34 +0000

    Spring AI

---
+++ backend/expense_tracker_backend/src/main/kotlin/mk/sorsix/com/expense_tracker_backend/repository/UserRepository.kt
@@ -0,0 +1,6 @@
+package mk.sorsix.com.expense_tracker_backend.repository
+
+import mk.sorsix.com.expense_tracker_backend.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Long>
